327023e42bd57ae8123e94cd78682f2606986fb8
nlin1027 <nathan.lin.chungking@gmail.com>
2026-07-11 08:35:45 -0400

    added simulation of users to generate way more traffic instead of a single request

--- single-process/main.py
+++ single-process/main.py
@@ -12,11 +12,24 @@ async def request(session, url):
     except (aiohttp.ClientError, asyncio.TimeoutError) as e:
         latency = time.perf_counter() - start
         return None, latency, e
+    
+async def simulate_user(session, url, duration, results):
+    while time.monotonic() < duration:
+        result, latency, error = await request(session, url)
+        results.append(f"status: {result} | latency: {latency} | error: {error}")
 
 async def main():
     url = "http://localhost:3000/average_list"
-    async with aiohttp.ClientSession() as session:
-        result, latency, error = await request(session, url)
-        print(f"status: {result} | latency: {latency} | error: {error}")
+    duration = 10
+    users = 25
+    results = []
+    timeout = aiohttp.ClientTimeout(total=10)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        tasks = []
+        duration = time.monotonic() + duration
+        for i in range(users):
+            tasks.append(asyncio.create_task(simulate_user(session, url, duration, results)))
+        await asyncio.gather(*tasks)
+    print(results)
 
 asyncio.run(main())
